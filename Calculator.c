bc47e5b99d7b513ab0b5d4137fc38f2dd8f15b42
anand <anandkumar9547@gmail.com>
2023-10-15 22:43:02 +0530

    3 files are added

---
+++ Calculator.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+    int main(){
+        int first,second;
+        scanf("%d",&first);
+        printf("5");
+        printf("first value=%d",first);
+        scanf("%d",&second);
+         printf("Second value=%d",second);
+        
+        return 0;
+    }
